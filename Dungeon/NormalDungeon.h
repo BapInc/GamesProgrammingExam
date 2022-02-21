024db826a138f34bb092a6bedf4a9fa3f1ec23cc
Pedro <pedro.rainha.dev@gmail.com>
2022-02-21 17:48:27 +0100

    cleaner code for dungeon generation

--- Dungeon/NormalDungeon.h
+++ Dungeon/NormalDungeon.h
@@ -15,27 +15,30 @@ protected:
 	bool generateRoom(int& width, int& height, RoomType type = RANDOMROOM);
 	void generateRoomConnections() override;
 	void findVisibleRooms();
-	void createFloor(int x, int y);
-	void createWall(int x, int y);
+
+
 	void connectRooms();
 	void generateCorridor(int roomOne, int roomTwo);
 	void generateCorridorAxis(bool first, int axis, int roomOne, int roomTwo, glm::ivec2& distance, glm::ivec2 startPos);
+	
 	void generateWalls() override;
+
 	void generateContent() override;
 	void generateEnemies();
-	float CalculateDistance(glm::ivec2& v, glm::ivec2& w);
 
 private:
 
+	void createFloor(int x, int y);
+	void createWall(int x, int y);
+
+	float CalculateDistance(glm::ivec2& v, glm::ivec2& w);
 	void swap(float& x, float& y);
 	void sortCosts(std::vector<float>& mapkeys);
 	glm::ivec2 parseConnections(std::string& string);
-	//TODO: Maybe keep pointers to start/end and reward rooms
 
 	LevelState* levelState;
 
 private:
-	//TODO: Read these from a .json file
 
 	std::vector<std::shared_ptr<Room>> rooms;
 	std::vector<int> roomsConnected;
@@ -44,9 +47,10 @@ private:
 
 	float roomVisibilityDistance;
 	float scaleMultiplier;
+	float maxRoomFill;
 
 	int minDistanceBetweenRooms;
-	int maxIterations; // NUmber of times it can iterate without creating a room, if higher then breaks out of the while loop
+	int maxIterations; // Number of times it can iterate without creating a room, if higher then breaks out of the while loop
 
 	int minRoomWidth;
 	int maxRoomWidth;
